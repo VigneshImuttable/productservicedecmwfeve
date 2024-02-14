9d6c988dfa6996ce4d0eb82dd21e49ca5cda0dec
VigneshImuttable <vigneshofficial41@gmail.com>
2024-02-14 09:10:52 +0530

    Spring Hibernate Implementation

--- src/main/java/com/project/productservicedecmwfeve/repositories/ProductRepository.java
+++ src/main/java/com/project/productservicedecmwfeve/repositories/ProductRepository.java
@@ -4,6 +4,25 @@ import com.project.productservicedecmwfeve.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+//Here Product is the class fo4r which a table is created for its components
+//Long is the data type of the primary key
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByTitleContainingAllIgnoreCase(String word);
+
+    long deleteByTitle(String title);
+
+    List<Product> findByTitleAndDescriptionLikeOrderByDescription(String title, String description);
+
+
+    //Here we derived the product list based on a particular category id, "_: is used for derived queries
+    List<Product> findByCategory_Id(Long id);
+
+
+
+
 }
